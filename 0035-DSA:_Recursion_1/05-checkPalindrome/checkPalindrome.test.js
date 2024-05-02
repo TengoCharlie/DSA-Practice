9700ea26d0a7c0fd88ee2a96f42b32a068b39981
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-02 17:28:27 +0000

    Test Case timeout not working

--- 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.test.js
+++ 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.test.js
@@ -1,15 +1,21 @@
 const checkPalindrome = require("./checkPalindrome");
 
-describe("Check Palandrome Tests", () => {
-    const tests =  new Map([
-        ["naman", 1],
-        ["strings", 0]
+jest.setTimeout(1000);
+
+describe("Check Palindrome Tests", () => {
+    const tests = new Map([
+        // ["naman", 1],
+        // ["strings", 0],
+        ["testttdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdf", 1],
+        ["testttdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdftfhgsf", 1],
+        ["testttdfgsdfgsdfgsdfgdxfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdf", 1],
+        ["testttdfgsdfgsdfgsdfsdfgsgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdf", 1]
     ]);
 
-    tests.forEach((output, input) => {
-        it("Should Return same value", () => {
-            const result = checkPalindrome(input);
-            expect(result).toBe(output);
-        })
-    })
-})
+    tests.forEach((expected, input) => {
+        test.concurrent(`should return ${expected} for input "${input}"`, () => {
+                const result = checkPalindrome(input);
+                expect(result).toBe(expected);
+        });
+    });
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 
+TODO : Limit memory usage 
+
+space limit : 10 pow 6 of array length or 4mb
+Time limit: ? check whatsapp
