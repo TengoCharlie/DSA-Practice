f9d045d7d043ea36f7bb55d6116bab70eda6d378
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-02 17:55:46 +0000

    Bruteforce check palandrome

--- 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.js
+++ 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.js
@@ -1,8 +1,20 @@
 //param A : string
 //return an integer
 module.exports = function (A) {
-
+    return bruteForce(A);
 }
 
+function bruteForce(A) {
+    A = A.split(' ').join('');
+    // BF
+    let loopCount = 0;
+    const length = A.length;
 
+    if(length % 2 == 0) loopCount = length/2;
+    else loopCount = (length-1)/2
+    for (let i = 0, j = length-1; i < loopCount, j >= loopCount; i++, j--) {
+            if(A[i] != A[j]) return 0;
+    }
+    return 1;
+}
 
--- 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.test.js
+++ 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.test.js
@@ -4,7 +4,17 @@ const checkPalindrome = require("./checkPalindrome");
 describe("Check Palindrome Tests", () => {
     const tests = new Map([
         ["naman", 1],
-        ["strings", 0]
+        ["strings", 0],
+        ["racecar", 1],    // Typical Palindrome
+        ["hello", 0],      // Not a Palindrome
+        ["a", 1],          // Single Character
+        ["aa", 1],         // Two Characters, Palindrome
+        ["ab", 0],         // Two Characters, Not a Palindrome
+        ["", 1],           // Empty String
+        ["a".repeat(25000) + "b" + "a".repeat(25000), 1],  // Very Long Palindrome
+        ["a".repeat(49999) + "b", 0],  // Very Long Non-Palindrome
+        ["aba", 1],  // Palindrome with Mixed Casing (assumed lowercase input)
+        ["a man a plan a canal panama", 1]  // Palindrome with Spaces (assuming spaces are ignored)    
     ]);
     
     tests.forEach((expected, input) => {
