c7251f26730a68068eb3a64d05bf24dce560d78c
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-02 17:43:46 +0000

    Palandrome Question added

--- 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.js
+++ 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.js
@@ -1,26 +1,8 @@
 //param A : string
 //return an integer
 module.exports = function (A) {
-    return bruteForce(A);
+
 }
 
-function bruteForce(A){
-    // BF
-    for(let i = 0; i<A.length; i++){
-        for(let j = A.length-1; j>=0; j--){
-            // if(A[i] != A[j]) return 0;
-            for(let i = 0; i<A.length; i++){
-                for(let j = A.length-1; j>=0; j--){
-                    // if(A[i] != A[j]) return 0;
-                    for(let i = 0; i<A.length; i++){
-                        for(let j = A.length-1; j>=0; j--){
-                            // if(A[i] != A[j]) return 0;
-                        }
-                       }
-                }
-               }
-        }
-       }
-       return 1;
-}
+
 
--- 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.test.js
+++ 0035-DSA:_Recursion_1/05-checkPalindrome/checkPalindrome.test.js
@@ -1,17 +1,12 @@
 const checkPalindrome = require("./checkPalindrome");
 
-jest.setTimeout(1000);
 
 describe("Check Palindrome Tests", () => {
     const tests = new Map([
-        // ["naman", 1],
-        // ["strings", 0],
-        ["testttdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdf", 1],
-        ["testttdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdftfhgsf", 1],
-        ["testttdfgsdfgsdfgsdfgdxfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdf", 1],
-        ["testttdfgsdfgsdfgsdfsdfgsgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdf", 1]
+        ["naman", 1],
+        ["strings", 0]
     ]);
-
+    
     tests.forEach((expected, input) => {
         test.concurrent(`should return ${expected} for input "${input}"`, () => {
                 const result = checkPalindrome(input);
