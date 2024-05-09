c1be454cb03ca1c51493e09690635165cc2704b8
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-09 15:20:08 +0000

    0049/01 solution added

--- 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/01-mergeTwoSortedArrays/mergeTwoSortedArrays.js
+++ 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/01-mergeTwoSortedArrays/mergeTwoSortedArrays.js
@@ -1,3 +1,28 @@
 module.exports = function (A, B){
-    return 1;
+    return solution(A, B);
+}
+
+function solution(A, B){
+    const ans = new Array();
+    let i = 0, j = 0;
+
+    while(i<A.length && j<B.length){
+        const minValue = Math.min(A[i], B[j]);
+        ans.push(minValue);
+        if(minValue == A[i]) i++;
+        else j++;
+    }
+
+        while(j<B.length){
+            ans.push(B[j]);
+            j++;
+        }
+
+        while(i<A.length){
+            ans.push(A[i]);
+            i++;
+        }
+
+    return ans;
+
 }
--- 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/01-mergeTwoSortedArrays/mergeTwoSortedArrays.test.js
+++ 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/01-mergeTwoSortedArrays/mergeTwoSortedArrays.test.js
@@ -1,17 +1,18 @@
 
 const mergeTwoSortedArrays = require("./mergeTwoSortedArrays");
 
-describe("Count Pair Difference Tests", () => {
+describe("Merge Two Sorted Array Tests", () => {
     const cases = [
-        [[4, 7, 9], [2, 11, 19], [2, 4, 7, 9, 11, 19]] 
+        [[4, 7, 9], [2, 11, 19], [2, 4, 7, 9, 11, 19]],
         [[1], [2], [1, 2]]
         // TODO
     ]
 
 
-    test.concurrent.each(cases)
-        ('On Input %s and %i should return %i ', (a, b, expected) => {
-            const result = mergeTwoSortedArrays(a, b);
-            expect(result).toBe(expected);
-        });
+    cases.forEach((el) => {
+        test(`Should Return ${el[2]}, on Input A = ${el[0]} and B = ${el[1]}`, () => {
+            const result = mergeTwoSortedArrays(el[0], el[1]);
+            expect(result).toEqual(el[2]);
+        })
+    })
 });
