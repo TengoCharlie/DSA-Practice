e670a2e639d1afe302bf1193d1bf767c5cc7f1e2
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-09 15:54:11 +0000

    0049/02 Brute Force Solution Added

--- 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/02-inversionCountInAnArray/inversionCount.js
+++ 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/02-inversionCountInAnArray/inversionCount.js
@@ -1,3 +1,15 @@
 module.exports = function (A){
-    return 1;
+    return bruteForce(A);
+}
+
+function bruteForce(A){
+    let count = 0;
+    for(let i = 0; i<A.length-1; i++){
+        for(let j = 1; j<A.length; j++){
+            if(i<j && A[i] > A[j]){
+                count += 1;
+            }
+        }
+    }
+    return count % (Math.pow(10, 9) + 7);
 }
--- 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/02-inversionCountInAnArray/inversionCount.test.js
+++ 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/02-inversionCountInAnArray/inversionCount.test.js
@@ -3,7 +3,7 @@ const inversionCount = require("./inversionCount");
 
 describe("Inversion Count In An Array Tests", () => {
     const cases = [
-        [[1, 2, 3], 1],
+        [[1, 3, 2], 1],
         [[3, 4, 1, 2], 4]
         // TODO
     ]
