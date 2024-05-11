cef22e9ac065d9e601fa3855eb8e0d5981ddc656
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-11 19:36:00 +0000

    0049/03 solution added

--- 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/03-countSort/countSort.js
+++ 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/03-countSort/countSort.js
@@ -1,3 +1,47 @@
-module.exports = function (A){
-    return 1;
+module.exports = function (A) {
+    return solution(A);
 }
+
+function solution(A) {
+    // Find Min Max value in the array
+    let smallest = A[0];
+    let largest = A[0];
+    // TC = O(n)
+    for (let i = 1; i < A.length; i++) {
+        smallest = Math.min(smallest, A[i]);
+        largest = Math.max(largest, A[i]);
+    }
+    // Convert the range to length of array
+    const range = largest - smallest + 1;
+    const countArr = new Array(range).fill(0, 0, range);
+    
+    // Count Sort array
+     // TC = O(n)
+    for (let i = 0; i < A.length; i++) {
+        const activeIndex = A[i]-smallest;
+        countArr[activeIndex] += 1;
+    }
+    // Cummulative count conversion to make stable sort
+
+     // TC = O(r)
+    for(let i = 1; i<range; i++){
+        countArr[i] += countArr[i-1];
+    }
+
+    const sorted = new Array(A.length);
+
+    // output sort array
+     // TC = O(n)
+    for(let i = A.length-1;i>=0; i--){
+        const countIndex = A[i]-smallest;
+        const sortedIndex = countArr[countIndex] - 1;
+        sorted[sortedIndex] = A[i];
+        countArr[countIndex]--;
+    }
+
+    return sorted;
+
+}
+
+
+// TC = O(n+n+n+r) => o(3n+r) = O(n+r)
--- 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/03-countSort/countSort.test.js
+++ 0049-DSA:_Sorting_1:_Count_Sort_&_Merge_Sort/03-countSort/countSort.test.js
@@ -4,7 +4,8 @@ const countSort = require("./countSort");
 describe("Count sort of An Array Tests", () => {
     const cases = [
         [[1, 3, 1], [1, 1, 3]],
-        [[4, 2, 1, 3], [1, 2, 3, 4]]
+        [[4, 2, 1, 3], [1, 2, 3, 4]],
+        [[6,3,3,6,7,8,7,3,7], [3, 3, 3, 6, 6, 7, 7, 7, 8]]
         // TODO
     ]
 
