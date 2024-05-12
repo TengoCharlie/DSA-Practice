1ce5157192d0b7856ba2b99b34e2f245c794022b
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-12 07:59:10 +0000

    0050/01 solution added

--- 0050-DSA:_Sorting_2:_Quick_Sort_&_Comparator_Problem/01-factorsSort/factorsSort.js
+++ 0050-DSA:_Sorting_2:_Quick_Sort_&_Comparator_Problem/01-factorsSort/factorsSort.js
@@ -1,3 +1,30 @@
-module.exports = function (A, B){
-    return 0
+module.exports = function (A){
+    return solution(A);
+}
+
+
+function solution(A){
+    A.sort((a, b) => {
+        if(getFactorsCount(a) === getFactorsCount(b)){
+            return a-b;
+        } else {
+            return getFactorsCount(a)-getFactorsCount(b)
+        }
+    })
+    return A;
+}
+
+function getFactorsCount(val){
+    let i = Math.floor(Math.sqrt(val));
+    let count = 0;
+    while(i > 0){ 
+        if(val%i == 0){
+            if(val/i == i){
+                count++;
+            } else count+=2;
+        }
+        i--;
+    }
+    console.log(count, val )
+    return count;
 }
--- 0050-DSA:_Sorting_2:_Quick_Sort_&_Comparator_Problem/01-factorsSort/factorsSort.test.js
+++ 0050-DSA:_Sorting_2:_Quick_Sort_&_Comparator_Problem/01-factorsSort/factorsSort.test.js
@@ -3,7 +3,9 @@ const factorsSort = require("./factorsSort");
 
 describe("Factors Sort Tests", () => {
     const cases = [
-   
+        [[6, 8, 9], [9, 6, 8]],
+        [[2, 4, 7], [2, 7, 4]],
+        [[36,13,13,26,37,28,27,43,7], [7, 13, 13, 37, 43, 26, 27, 28, 36]]
         // TODO
     ]
 
