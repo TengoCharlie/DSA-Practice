8f20e99484994ef26f9ec36b0ede612b8187242b
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-13 09:35:52 +0000

    0051 04 solution added

--- src/0051-DSA:_Searching_1:_Binary_Search_On_Array/04-singleElementInSortedArray/singleElementInSortedArray.js
+++ src/0051-DSA:_Searching_1:_Binary_Search_On_Array/04-singleElementInSortedArray/singleElementInSortedArray.js
@@ -3,5 +3,31 @@ module.exports = function (A){
 }
 
 function solution(A){
-    
+    let lo = 1;
+    let hi = A.length-2;
+
+    // Base Cases
+    if(A[0] !== A[1]) return A[0];
+    if(A[A.length-1] !== A[A.length-2]) return A[A.length-1];
+
+    while(lo<=hi){
+        const mid = Math.floor(lo + (hi-lo)/2);
+
+        if(A[mid] != A[mid-1] && A[mid] != A[mid+1]){
+            return A[mid];
+        } else if(
+            mid % 2 == 1 && A[mid] == A[mid+1] ||
+            mid % 2 == 0 && A[mid-1] == A[mid]
+        ) {
+            hi = mid-1
+        } else {
+            lo = mid +1
+        }
+    }
+
+    return -1;
 }
+
+
+// Observation 
+//  All the values right side of the unique value having first instance at odd index and all the values left side of the unique value having first element at even index.
