1d04df2275c6ff40aeea82a52899474503da5750
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-13 07:00:02 +0000

    0051/04 Question Added

---
+++ 0051-DSA:_Searching_1:_Binary_Search_On_Array/04-singleElementInSortedArray/singleElementInSortedArray.test.js
@@ -0,0 +1,18 @@
+
+const singleElementInSortedArray = require("./singleElementInSortedArray");
+
+describe("Factors Sort Tests", () => {
+    const cases = [
+        [[1, 1, 7], 7],
+        [[2, 3, 3], 2]
+        // TODO
+    ]
+
+
+    cases.forEach((el) => {
+        test(`Should Return ${el[1]}, on Input A = ${el[0]}}`, () => {
+            const result = singleElementInSortedArray(el[0]);
+            expect(result).toEqual(el[1]);
+        })
+    })
+});
