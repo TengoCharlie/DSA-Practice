ff05a0d86ae1e7fe5f3539b2edeeb83f4237eb04
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-13 06:54:38 +0000

    0051/01 Solution Added

--- 0051-DSA:_Searching_1:_Binary_Search_On_Array/01-sortedInsertPosition/sortedInsertPosition.js
+++ 0051-DSA:_Searching_1:_Binary_Search_On_Array/01-sortedInsertPosition/sortedInsertPosition.js
@@ -1,3 +1,27 @@
 module.exports = function (A, B){
-    return 0
+    return solution(A, B);
+}
+
+function solution(A, B){
+    let lo = 1;
+    let hi = A.length-2
+    let ans = -1;
+
+    if(A[0] >= B) return 0;
+    if(A[A.length-1] == B) return (A.length-1);
+    if(A[A.length-1] < B) return A.length;
+
+    while(lo<=hi){
+        const mid = Math.floor(lo + (hi-lo)/2);
+        if(A[mid] == B){
+            return mid;
+        } else if(A[mid] > B){
+            hi = mid-1;
+            ans = mid;
+        } else {
+            lo = mid+1;
+        }
+    }
+
+    return ans;
 }
