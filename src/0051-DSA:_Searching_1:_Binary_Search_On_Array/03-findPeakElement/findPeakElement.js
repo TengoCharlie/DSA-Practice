3a62087a641f0507467fce93fb91f9d069cbf4ca
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-13 15:20:30 +0000

    0051/03 Solution Added

--- src/0051-DSA:_Searching_1:_Binary_Search_On_Array/03-findPeakElement/findPeakElement.js
+++ src/0051-DSA:_Searching_1:_Binary_Search_On_Array/03-findPeakElement/findPeakElement.js
@@ -1,3 +1,25 @@
 module.exports = function (A){
-    return 0
+    return solution(A);
+}
+
+function solution(A){
+    let N = A.length-1;
+    if(N<1) return A[N];
+    let lo=1;
+    let hi = N-1;
+
+    if(A[0] >= A[1]) return A[0];
+    else if(A[N-1] <= A[N]) return A[N];
+
+    while(lo <= hi){
+        const mid = Math.floor(lo + (hi-lo)/2);
+
+        if(A[mid] > A[mid-1] && A[mid] > A[mid+1]){
+            return A[mid];
+        } else if(A[mid-1] < A[mid] && A[mid] < A[mid+1]){
+            lo = mid+1;
+        } else {
+            hi = mid-1
+        }
+    }
 }
