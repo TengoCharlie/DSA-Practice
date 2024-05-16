0a3f432e6b19aaa0adf25e8f85dd0d0a2b103ba8
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-16 13:33:42 +0000

    0052 03 solution added

--- src/0052-DSA:_Searching_2:_Binary_Search_Problem/03-squareRootInteger/squareRootInteger.js
+++ src/0052-DSA:_Searching_2:_Binary_Search_Problem/03-squareRootInteger/squareRootInteger.js
@@ -1,3 +1,22 @@
 module.exports = function (A){
-    return 0
+    return optimise(A);
+}
+
+function optimise(A) { 
+    if(A <= 1) return A;
+
+    let l = 1;
+    let h = A-1;
+    let ans = 0;
+
+    while(l<=h){
+        const mid = Math.floor(l + (h - l)/2);
+        const sqre = mid*mid;
+        if(sqre== A) return mid;
+        else if(sqre < A) {
+            ans = mid;
+            l = mid+1;
+        } else h = mid-1;
+    }
+    return ans;
 }
