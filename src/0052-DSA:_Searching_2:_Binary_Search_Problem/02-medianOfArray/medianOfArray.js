39e0a6610c8f6311cf28f4aa5415de5e87838d3b
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-16 18:51:20 +0000

    0052 02 Solution Added

--- src/0052-DSA:_Searching_2:_Binary_Search_Problem/02-medianOfArray/medianOfArray.js
+++ src/0052-DSA:_Searching_2:_Binary_Search_Problem/02-medianOfArray/medianOfArray.js
@@ -1,3 +1,51 @@
 module.exports = function (A, B){
-    return 0
+    return parseFloat(optimisation(A, B));
 }
+  
+  
+  function optimisation(A, B){
+      if(A.length >= B.length){
+          return solve(A, B, A.length, B.length);
+      } else {
+          return solve(B, A, B.length, A.length);
+      }
+  }
+  
+  function solve(A, B, N, M){
+      // Find X
+      const x = parseInt((N+M)/2);
+  
+      let l = 0;
+      let r = x;
+  
+      const INT_MIN = Number.MIN_SAFE_INTEGER;
+  const INT_MAX = Number.MAX_SAFE_INTEGER;
+  
+      while(l<=r){
+          const mid = Math.floor((l + (r-l)/2));
+  
+          const c1 = mid;
+          const c2 = x-mid;
+  
+          const l1 = c1 >= 1 ? A[c1-1] : INT_MIN, r1 = c1 < N ? A[c1] : INT_MAX;
+          const l2 = c2 >= 1 ? B[c2-1] : INT_MIN, r2 = c2 < M ? B[c2] : INT_MAX;
+  
+          if(l1<=r2 && l2<=r1){
+              if((N+M) % 2 == 1){
+                  return Math.min(r1, r2);
+              } else {
+                  const max = Math.max(l1, l2);
+                  const min = Math.min(r1, r2);
+                  const avg = parseFloat((min + max) / 2)
+                  return avg;
+              }
+          } else if(l1>r2){
+              r = mid-1;
+          } else {
+              l = mid+1;
+          }
+      }
+      return -1;
+  }
+    
+  
--- src/0052-DSA:_Searching_2:_Binary_Search_Problem/02-medianOfArray/medianOfArray.test.js
+++ src/0052-DSA:_Searching_2:_Binary_Search_Problem/02-medianOfArray/medianOfArray.test.js
@@ -4,7 +4,9 @@ const medianOfArray = require("./medianOfArray");
 describe("Median of Array Tests", () => {
     const cases = [
         [[1, 4, 5], [2, 3], 3.0],
-        [[1, 2, 3], [4], 2.5]
+        [[1, 2, 3], [4], 2.5],
+        [[1, 4, 5, 7, 9, 14, 18], [3, 4, 6, 8, 10, 12, 15, 16], 8.0],
+        [[], [20], 20.0]
         // TODO
     ]
 
