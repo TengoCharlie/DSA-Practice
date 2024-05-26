f047d5a34e9807e05e4bfdd8ace021238dd49ded
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-05-26 11:36:19 +0000

    0053 01 solution pending

--- src/0053-DSA:_Searching_3:_Binary_Search_On_Answers/01-aggressiveCows/aggressiveCows.js
+++ src/0053-DSA:_Searching_3:_Binary_Search_On_Answers/01-aggressiveCows/aggressiveCows.js
@@ -1,3 +1,37 @@
 module.exports = function (A, B){
-    return 0
+    return solution(A, B);
+}
+
+function solution(A, B){
+    let l = 1;
+    let r = A[A.length-1] - A[0];
+    let ans = 0;
+
+    while(l<=r){
+        const mid = Math.floor(l + (r-l)/2);
+        const possible = aggressiveCows(A, B, mid);
+        if(possible){
+            ans = mid;
+            l = mid+1;
+        } else {
+            r = mid-1;
+        }
+    }
+    return ans;
+}
+
+
+function aggressiveCows(A, B, mid){
+    let count = 1;
+    let last_pos = A[0];
+    for(let i = 1; i <A.length; i++){
+        if(A[i] - last_pos >= mid){
+            count++;
+            last_pos = A[i];
+            if(count === B){
+                return true;
+            }
+        }
+    }
+    return false;
 }
