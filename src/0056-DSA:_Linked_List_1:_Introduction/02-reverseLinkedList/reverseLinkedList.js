80dda3cc1c17a2878c931fc8676db2b19cf4f13d
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-01 03:36:01 +0530

    0056 02 solution added

--- src/0056-DSA:_Linked_List_1:_Introduction/02-reverseLinkedList/reverseLinkedList.js
+++ src/0056-DSA:_Linked_List_1:_Introduction/02-reverseLinkedList/reverseLinkedList.js
@@ -1,3 +1,19 @@
-module.exports = function (A){
-    return A
+module.exports = function (A) {
+    return solution(A);
+};
+
+
+function solution(A) {
+    let head = A;
+    let temp = head;
+    let prev = null;
+    let current = temp;
+    while (temp != null) {
+        temp = temp.next;
+        current.next = prev;
+        prev = current;
+        current = temp;
+    }
+    head = prev;
+    return head;
 }
