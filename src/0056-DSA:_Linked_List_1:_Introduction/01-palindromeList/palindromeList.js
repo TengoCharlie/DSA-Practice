fff33dcbdc939d3714ac45aef956c02d9e4be763
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-01 21:22:12 +0530

    0056 01 bruteforce solution added

--- src/0056-DSA:_Linked_List_1:_Introduction/01-palindromeList/palindromeList.js
+++ src/0056-DSA:_Linked_List_1:_Introduction/01-palindromeList/palindromeList.js
@@ -1,3 +1,54 @@
-module.exports = function (A){
-    return A
+module.exports = function (A) {
+    return bruteForce(A);
+}
+
+function bruteForce(head) {
+    let size = getSize(head);
+    const midIndex = size % 2 ? (size + 1) / 2 : size / 2;
+    let midHead = getMidHead(head, midIndex+1);
+    const midReverse = reverseLinkedList(midHead);
+    const result = isPalindrome(head, midReverse);
+    return result;
+}
+
+function isPalindrome(head, leg) {
+    while (leg != null) {
+        if (head.data != leg.data) return 0;
+        head = head.next;
+        leg = leg.next;
+    }
+    return 1;
+}
+
+function getSize(head) {
+    let temp = head;
+    let count = 0;
+    while (temp != null) {
+        temp = temp.next;
+        count++;
+    }
+    return count;
+}
+
+function getMidHead(head, midIndex) {
+    let count = 1;
+    let temp = head;
+    while (count < midIndex) {
+        temp = temp.next;
+        count++;
+    }
+    return temp;
+}
+
+function reverseLinkedList(head) {
+    let temp = head;
+    let prev = null;
+    let current = temp;
+    while (temp != null) {
+        temp = temp.next;
+        current.next = prev;
+        prev = current;
+        current = temp
+    }
+    return prev;
 }
