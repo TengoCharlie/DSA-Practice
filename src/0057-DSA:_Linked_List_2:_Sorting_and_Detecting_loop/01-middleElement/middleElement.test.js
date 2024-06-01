1869d1eab89aa4abca58ec70c8886d9e0b30906c
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-02 04:43:24 +0530

    0057 01 Question Added

---
+++ src/0057-DSA:_Linked_List_2:_Sorting_and_Detecting_loop/01-middleElement/middleElement.test.js
@@ -0,0 +1,33 @@
+const middleElement = require("./middleElement");
+
+describe("Middle Element of Linked List Tests", () => {
+    const cases = [
+        // Test cases
+        {
+            input: [1, 2, 3, 4, 5],
+            expected: 3
+        },
+        {
+            input: [1, 5, 6, 2, 3, 4],
+            expected: 2
+        }
+    ];
+
+    function LNode(data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    cases.forEach(({input, expected}) => {
+        test(`Should return ${expected} as the middle element for the linked list ${input}`, () => {
+            let head = new LNode(input[0]);
+            let temp = head;
+            for (let i = 1; i < input.length; i++) {
+                temp.next = new LNode(input[i]);
+                temp = temp.next;
+            }
+            const result = middleElement(head);
+            expect(result).toEqual(expected);
+        });
+    });
+});
