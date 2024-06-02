02c7d073c5232f7b086ca17e714c7fb30d3d717a
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-02 19:00:40 +0530

    0057 02 Solution Added

--- src/0057-DSA:_Linked_List_2:_Sorting_and_Detecting_loop/02-mergeSortedLinkedList/mergeSortedLinkedList.js
+++ src/0057-DSA:_Linked_List_2:_Sorting_and_Detecting_loop/02-mergeSortedLinkedList/mergeSortedLinkedList.js
@@ -1,3 +1,44 @@
-module.exports = function (A) {
-    return A
+const linkedListNode = require("../../../utils/linkedListNode");
+
+module.exports = function (A, B) {
+  return solution(A, B);
+};
+
+function solution(A, B) {
+  if (A == null) return B;
+  if (B == null) return A;
+
+  let head = new linkedListNode(-1);
+  let h1 = A;
+  let h2 = B;
+  // Same Logic But with extra line of code
+  //   let head = h1;
+  //   let h1 = h1.next;
+  //   if (A.data > B.data) {
+  //     h1 = head;
+  //     head = B;
+  //     h2 = head.next;
+  //   }
+  let tail = head;
+
+  while (h1 != null && h2 != null) {
+    if (h1.data <= h2.data) {
+      tail.next = h1;
+      tail = h1;
+      h1 = h1.next;
+    } else {
+      tail.next = h2;
+      tail = h2;
+      h2 = h2.next;
+    }
+  }
+
+  if (h1 == null) {
+    tail.next = h2;
+  }
+
+  if (h2 == null) {
+    tail.next = h1;
+  }
+  return head.next;
 }
