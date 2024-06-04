353274a16dd4b4336853ee191a57fefcd78f8d6d
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-04 11:32:16 +0530

    0057 04 Solution Added

--- src/0057-DSA:_Linked_List_2:_Sorting_and_Detecting_loop/04-removeLoop/removeLoop.js
+++ src/0057-DSA:_Linked_List_2:_Sorting_and_Detecting_loop/04-removeLoop/removeLoop.js
@@ -3,5 +3,20 @@ module.exports = function (A) {
 }
 
 function solution(A) {
-
+    let slow = A;
+    let fast = A;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            fast = A;
+            while (slow.next != fast.next) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            slow.next = null;
+            return A;
+        }
+    }
+    return A;
 }
