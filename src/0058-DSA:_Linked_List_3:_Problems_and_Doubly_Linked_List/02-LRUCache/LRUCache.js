54556d0e5d3fe7618eb3015e9db5786a34a3f0c3
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-09 18:58:19 +0530

    0058 02 solution Added(More optimization pending)

--- src/0058-DSA:_Linked_List_3:_Problems_and_Doubly_Linked_List/02-LRUCache/LRUCache.js
+++ src/0058-DSA:_Linked_List_3:_Problems_and_Doubly_Linked_List/02-LRUCache/LRUCache.js
@@ -1,17 +1,91 @@
-// Constructor : Initializes the LRUCache with capacity 
+// NOTE: This solution Is working But it needs Improvisation, hence this will be updated in next commit
+// Constructor : Initializes the LRUCache with capacity
 module.exports = {
-    LRUCache: function(capacity){
-      
-      return {
-        // get function returns an integer
-        get: function(key){
-        
-        },
-        // set returns nothing
-	    set: function(key, value){
-	
-        }
-      }
-    }
-}
+  LRUCache: function (capacity) {
+    let valMap = new Map();
+    let nodeMap = new Map();
+    let head = null;
+    let tail = null;
+    let size = capacity;
 
+    function addNewNode(key, value) {
+      if (size <= 0) {
+        nodeMap.delete(tail.data);
+        valMap.delete(tail.data);
+        if (tail == head) {
+          tail = null;
+          head = null;
+        } else {
+          tail.prev.next = null;
+          tail = tail.prev;
+        }
+        size++;
+      }
+
+      const temp = new DLNode(key);
+      if (head) head.prev = temp;
+      temp.next = head;
+      head = temp;
+      head.prev = null;
+
+      valMap.set(key, value);
+      nodeMap.set(key, temp);
+      if (size >= capacity) {
+        tail = head;
+      }
+      size--;
+    }
+
+    function updateNode(key, value = null) {
+      const temp = nodeMap.get(key);
+      if (temp == head) {
+        if (value) {
+          valMap.set(key, value);
+          return;
+        } else return valMap.get(key);
+      }
+      if (temp == tail) {
+        tail = temp.prev;
+      }
+      if (temp.prev) temp.prev.next = temp.next;
+      if (temp.next) temp.next.prev = temp.prev;
+
+      head.prev = temp;
+      temp.next = head;
+      head = temp;
+      head.prev = null;
+
+      if (value) valMap.set(key, value);
+      else return valMap.get(key);
+    }
+
+    return {
+      // get function returns an integer
+      get: function (key) {
+        if (!nodeMap.has(key)) {
+          return -1;
+        } else {
+          const res = updateNode(key);
+          return res;
+        }
+      },
+      // set returns nothing
+      set: function (key, value) {
+        if (!capacity) return null;
+        if (!nodeMap.has(key)) {
+          addNewNode(key, value);
+        } else {
+          updateNode(key, value);
+        }
+      },
+    };
+  },
+};
+
+class DLNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
