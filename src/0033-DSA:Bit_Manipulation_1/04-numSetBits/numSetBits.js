05ebf0954bfb03d6442ee2df11727e11780a0267
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-11 19:00:59 +0530

    Solution added 0033 04

--- src/0033-DSA:Bit_Manipulation_1/04-numSetBits/numSetBits.js
+++ src/0033-DSA:Bit_Manipulation_1/04-numSetBits/numSetBits.js
@@ -1,5 +1,13 @@
-module.exports = function (A) {
-    
-};
-
-
+module.exports = function(A){
+           let ans = 0;
+           for(let i = 0; i<32; i++){
+               if(checkBit(A, i)) ans++;
+           }
+           return ans;
+       }
+   
+   
+   function checkBit(A, B){
+       if(A&(1<<B)) return true;
+       else return false;
+   }
