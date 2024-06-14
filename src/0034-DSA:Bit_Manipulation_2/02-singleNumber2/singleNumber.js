b80222456a60a9c20bb74b46bbc1e9966f89ecaa
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-14 15:22:47 +0530

    Solution added 0034 02

--- src/0034-DSA:Bit_Manipulation_2/02-singleNumber2/singleNumber.js
+++ src/0034-DSA:Bit_Manipulation_2/02-singleNumber2/singleNumber.js
@@ -1,2 +1,15 @@
 module.exports = function (A) {
+    let ans = 0;
+    for(let i = 0; i<32; i++){
+        let cnt = 0;
+        for(let j = 0; j<A.length; j++){
+            if((A[j] & (1 << i)) > 0){
+                cnt++;
+            }
+        }
+        if(cnt % 3 == 1){
+            ans = ans | (1 << i);
+        }
+    }
+    return ans;
 };
