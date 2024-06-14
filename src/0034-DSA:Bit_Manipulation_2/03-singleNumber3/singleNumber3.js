65c4440d5efca3e946cc5d8f9978b901efb42098
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-15 02:38:14 +0530

    Solution Added 0034 03

--- src/0034-DSA:Bit_Manipulation_2/03-singleNumber3/singleNumber3.js
+++ src/0034-DSA:Bit_Manipulation_2/03-singleNumber3/singleNumber3.js
@@ -1,2 +1,35 @@
 module.exports = function (A) {
+  return solution(A);
 };
+
+function solution(A) {
+  const xorValue = getXorVal(A);
+  console.log(xorValue);
+  const setIndex = getSetBitIndex(xorValue);
+  console.log(setIndex);
+  let setAns = 0;
+  let unsetAns = 0;
+  for (let i = 0; i < A.length; i++) {
+    if ((A[i] & (1 << setIndex)) > 0) {
+      setAns = setAns ^ A[i];
+    } else {
+      unsetAns = unsetAns ^ A[i];
+    }
+  }
+
+  return setAns > unsetAns ? [unsetAns, setAns] : [setAns, unsetAns];
+}
+
+function getSetBitIndex(val) {
+  for (let i = 0; i < 32; i++) {
+    if ((val & (1 << i)) > 0) return i;
+  }
+}
+
+function getXorVal(A) {
+  let ans = 0;
+  for (let i = 0; i < A.length; i++) {
+    ans = ans ^ A[i];
+  }
+  return ans;
+}
