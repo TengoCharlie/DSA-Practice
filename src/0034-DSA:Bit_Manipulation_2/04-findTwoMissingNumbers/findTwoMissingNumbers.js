88d1dae4d0127573ddde8a128bc51ac72c8b753d
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-15 13:13:21 +0530

    Solution Added 0034 04

--- src/0034-DSA:Bit_Manipulation_2/04-findTwoMissingNumbers/findTwoMissingNumbers.js
+++ src/0034-DSA:Bit_Manipulation_2/04-findTwoMissingNumbers/findTwoMissingNumbers.js
@@ -1,2 +1,44 @@
 module.exports = function (A) {
+    return solution(A);
 };
+
+function solution(A){
+    let xorValue = getXorValue(A);
+    let setBit = getSetBit(xorValue);
+    let setAns = 0;
+    let unsetAns = 0;
+    for (let i = 0; i < A.length; i++){
+        if ((A[i] & (1 << setBit)) > 0) {
+            setAns = setAns ^ A[i];
+        } else {
+            unsetAns = unsetAns ^ A[i];
+        }
+    }
+    for (i = 1; i <= A.length + 2; i++){
+        if ((i & (1 << setBit)) > 0) {
+            setAns = setAns ^ i;
+        } else {
+            unsetAns = unsetAns ^ i;
+        }
+    }
+
+    return setAns > unsetAns ? [unsetAns, setAns] : [setAns, unsetAns];
+
+}
+
+function getSetBit(val) {
+    for (let i = 0; i < 32; i++){
+        if ((val & (1 << i)) > 0) return i;
+    }
+}
+
+function getXorValue(A) {
+    let xorValue = 0;
+    for (let i = 0; i < A.length; i++){
+        xorValue = xorValue ^ A[i];
+    }
+    for (let i = 1; i <= A.length + 2; i++){
+        xorValue = xorValue ^ i
+    }
+    return xorValue;
+}
