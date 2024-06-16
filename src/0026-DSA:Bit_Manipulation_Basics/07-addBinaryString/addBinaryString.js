ec3d77c2dfb6f970b6bbdab64c0fbca0e0c7ba0d
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-06-16 20:54:01 +0530

    Solution Added 0026 07

--- src/0026-DSA:Bit_Manipulation_Basics/07-addBinaryString/addBinaryString.js
+++ src/0026-DSA:Bit_Manipulation_Basics/07-addBinaryString/addBinaryString.js
@@ -1,3 +1,42 @@
 module.exports = function (A, B) {
-    
-}
+       let val1 = 0;
+       let val2 = 0;
+       let carry = 0;
+       let sum = 0;
+       let ans = [];
+   
+       if (A.length > B.length) {
+           size = A.length+1
+       }
+       else {
+           size = B.length+1;
+       }
+       B = B.padStart(size, '0');
+       A = A.padStart(size, '0');
+       for(let i = 0; i<size; i++){
+           ans[i] = 0;
+       }
+   
+       let i = 1;
+   
+       while (i <= size) {
+           val1 = parseInt(A[size - i])
+           val2 = parseInt(B[size - i]);
+           [sum, carry] = sumOfBinary(carry, val1, val2);
+           ans[size - i] = sum;
+           i++;
+       }
+       let res = '';
+       ans.forEach((el, i) => {
+           if (i == 0 && el < 1) {
+           } else {
+               res = res.concat(el);
+           }
+       })
+       return res;
+   }
+   
+   function sumOfBinary(carry, val1, val2) {
+       const sum = carry + val1 + val2;
+       return [Math.floor(sum % 2), Math.floor(sum / 2)];
+   }
