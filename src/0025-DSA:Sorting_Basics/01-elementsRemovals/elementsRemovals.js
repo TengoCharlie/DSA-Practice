0810fcfea522c727cc4250d3b8f54999d551161e
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-07-05 01:32:15 +0530

    Solution Added 0025 01

--- src/0025-DSA:Sorting_Basics/01-elementsRemovals/elementsRemovals.js
+++ src/0025-DSA:Sorting_Basics/01-elementsRemovals/elementsRemovals.js
@@ -1,1 +1,21 @@
-module.exports = function (A) { }
+module.exports = function (A) {
+    let sortedArray = A.ssort();
+    let ans = 0;
+    for (let i = 0; i < sortedArray.length; i++){
+        ans += sortedArray[i] * (i+1);
+    }
+    return ans;
+}
+
+Array.prototype.ssort = function () {
+    let arr = this;
+    let lengthOfArray = arr.length;
+    for (let i = 1; i < lengthOfArray; i++){
+        let j = i - 1;
+        while (j < lengthOfArray && arr[j] < arr[j + 1]) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            j--;
+        }
+    }
+    return arr;
+}
