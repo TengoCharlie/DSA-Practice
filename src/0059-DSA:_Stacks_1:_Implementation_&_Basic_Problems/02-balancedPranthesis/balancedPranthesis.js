e21921dff442c6efc1aa29f41a7f8502f0fafe0b
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-07-05 23:31:11 +0530

    Solution Added 0059 02

--- src/0059-DSA:_Stacks_1:_Implementation_&_Basic_Problems/02-balancedPranthesis/balancedPranthesis.js
+++ src/0059-DSA:_Stacks_1:_Implementation_&_Basic_Problems/02-balancedPranthesis/balancedPranthesis.js
@@ -1,3 +1,60 @@
 module.exports = function (A) {
-    return A;
+  return solution(A);
+};
+
+function solution(A) {
+  let stk = new Stack();
+  const openB = ["[", "{", "("];
+  const closedB = ["]", "}", ")"];
+  for (let i = 0; i < A.length; i++) {
+    if (openB.includes(A[i])) {
+      stk.push(A[i]);
+    } else {
+      if (stk.isEmpty()) return 0;
+
+      const counterB = openB[closedB.indexOf(A[i])];
+      if (stk.top() != counterB) {
+        return 0;
+      } else {
+        stk.pop();
+      }
+    }
+  }
+
+  if (stk.isEmpty()) return 1;
+  else return 0;
+}
+
+class NNode {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.head = null;
+  }
+
+  push(value) {
+    let node = new NNode(value);
+    node.next = this.head;
+    this.head = node;
+  }
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty, cannot pop");
+    }
+    this.head = this.head.next;
+  }
+
+  top() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty, cannot pop");
+    }
+    return this.head.value;
+  }
+  isEmpty() {
+    return !this.head;
+  }
 }
